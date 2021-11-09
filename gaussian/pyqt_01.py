c907bca5bbc2759fb90888ef081bcce99e1ac1a3
Noah Vawter <diydsp@yahoo.com>
2021-11-09 12:24:59 -0500

    added simple callbacks

--- gaussian/pyqt_01.py
+++ gaussian/pyqt_01.py
@@ -4,8 +4,21 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 app = QApplication([])
 window = QWidget()
 layout = QVBoxLayout()
-layout.addWidget(QPushButton('Top'))
-layout.addWidget(QPushButton('Bottom'))
+
+Top_butt = QPushButton('Top')
+layout.addWidget( Top_butt )
+Bott_butt = QPushButton('Bottom')
+layout.addWidget( Bott_butt )
+
+def on_Top_butt_clicked():
+    print("top was pressed")
+    
+def on_Bott_butt_clicked():
+    print("bott was pressed")
+
+Top_butt.clicked.connect( on_Top_butt_clicked )
+Bott_butt.clicked.connect( on_Bott_butt_clicked )
+
 window.setLayout(layout)
 window.show()
 app.exec()
