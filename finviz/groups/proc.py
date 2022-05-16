ecf5a980b241ead051a7ae1acc5921fdd4416dba
Noah Vawter <diydsp@yahoo.com>
2022-05-16 10:35:00 -0400

    made proc.py more enterprise, but it needs the improved proc code from proc3.py

--- finviz/groups/proc.py
+++ finviz/groups/proc.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python3
 
+'''
+extract useful data from the "groups" data from finviz
+'''
+
 import json
 import pdb
+import argparse
+
+def readData( filename ):    
+    with open( filename) as fin:
+        rows = fin.readlines()
+    return rows
 
 
-with open('rows_groups.txt') as fin:
-    rows = fin.readlines()
+def extractGoodStuff( rows ):
+    st = rows[0]
+    
+    inside_brackets  = st.split('[')[-1].split(']')[0]
+    extracted = []
+    [ extracted.append( x ) for x in inside_brackets.split( '{' ) ]
+    return extracted
 
-st = rows[0]
+parser = argparse.ArgumentParser( description='control learner' )
+parser.add_argument( '--date',
+                    dest='date',
+                    required=True,
+                    help='e.g. 2022-05-13  the date to process' )
+                    
+if __name__ == '__main__':
+    args = parser.parse_args()
+    date = args.date
+    #if date is None:
+        
+    dataDir = f'{date}/data'    
+    procDir = f'{date}/proc'
 
-inside_brackets  = st.split('[')[-1].split(']')[0]
-[ print(f'{x}') for x in inside_brackets.split( '{' ) ]
+    dataFile = f'{dataDir}/rows_groups.txt'
+    rows = readData( dataFile )
+    extracted = extractGoodStuff( rows )
+
+    [ print(x) for x in extracted ]
+
+
